707eb0cfd00bc4a56765ffb2f4d91c1a282baaf4
Ayano <s.correa.wilches@gmail.com>
2020-04-05 01:49:52 +0700

    updtae1
    update1

---
+++ front-end/controllers/links.controller.js
@@ -0,0 +1,52 @@
+const linksCtrl = {};
+
+const pool = require('../database');
+
+linksCtrl.renderAddLink = (req, res) => {
+    res.render('links/add');
+};
+
+linksCtrl.addLink = async (req, res) => {
+    const {url} = req.body;
+
+    const newLink = {
+        id: req.user.id,
+        url,
+        connectionID: id+url
+    };
+    await pool.query('INSERT INTO Directories set ?', [newLink]);
+    req.flash('success', 'Link Saved Successfully');
+    res.redirect('/links');
+}
+
+linksCtrl.renderLinks = async (req, res) => {
+    const links = await pool.query('SELECT * FROM Directories WHERE userID = ?', [req.user.id]);
+    res.render('links/list', { links });
+}
+
+linksCtrl.deleteLink = async (req, res) => {
+    const { id } = req.params;
+    await pool.query('DELETE FROM Directories WHERE connectionID = ?', [id]);
+    req.flash('success', 'Link Removed Successfully');
+    res.redirect('/links');
+};
+
+linksCtrl.renderEditLink = async (req, res) => {
+    const { id } = req.params;
+    const links = await pool.query('SELECT * FROM Directories WHERE connectionID = ?', [id]);
+    console.log(links);
+    res.render('links/edit', {link: links[0]});
+};
+
+linksCtrl.editLink = async (req,res) => {
+    const { id } = req.params;
+    const { url} = req.body; 
+    const newLink = {
+        url
+    };
+    await pool.query('UPDATE Directories set ? WHERE connectionID = ?', [newLink, id]);
+    req.flash('success', 'Link Updated Successfully');
+    res.redirect('/links');
+}
+
+module.exports = linksCtrl;
